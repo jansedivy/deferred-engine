a3250fe33d0e674b8f9c4e3ef60eb058bd29c146
Jan Sedivy <jansedivy@abdoc.net>
2015-03-01 00:16:37 +0100

    add ambient shaders

---
+++ resources/shaders/ambientlight.frag.glsl
@@ -0,0 +1,17 @@
+#version 330
+
+uniform sampler2D diffuseTexture;
+
+uniform vec3 lightColor;
+
+out vec4 fragColor;
+
+in vec2 pos;
+
+void main() {
+  vec2 uv = pos * 0.5 + 0.5;
+
+  vec3 diffuseColor = texture(diffuseTexture, uv).xyz;
+
+  fragColor = vec4(diffuseColor, 1.0) * vec4(lightColor, 1.0);
+}
